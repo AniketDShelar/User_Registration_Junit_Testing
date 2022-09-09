e525e2fe2fabe338491e5e1202a87eb380605ff6
AniketDShelar <theaniketshelar@gmail.com>
2022-09-10 01:49:22 +0530

    custom exception

--- src/com/intellijide/userregistrationtesting/UserRegistrationsTest.java
+++ src/com/intellijide/userregistrationtesting/UserRegistrationsTest.java
@@ -3,8 +3,6 @@ package com.intellijide.userregistrationtesting;
 import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class UserRegistrationsTest {
     UserRegistrations test;
     void initialize(){
@@ -12,33 +10,58 @@ public class UserRegistrationsTest {
     }
 
     @Test
-    void givenFirstNameInCorrectFormatShouldReturnTrue() {
+    void givenFirstNameInCorrectFormatShouldReturnTrue(){
         test = new UserRegistrations();
-        boolean actual = test.firstName("Aniket");
+        boolean actual = false;
+        try {
+            actual = test.firstName("aniket");
+        } catch (Exception e) {
+            System.out.println("Invalid Input "+e.getMessage());
+        }
         Assertions.assertEquals(true,actual);
     }
     @Test
-    void givenLastNameInCorrectFormatShouldReturnTrue() {
+    void givenLastNameInCorrectFormatShouldReturnTrue(){
         test = new UserRegistrations();
-        boolean actual = test.lastName("Shelar");
+        boolean actual = false;
+        try {
+            actual = test.lastName("shelar");
+        } catch (Exception e) {
+            System.out.println("Invalid Input "+e.getMessage());
+        }
         Assertions.assertEquals(true,actual);
     }
     @Test
-    void givenEmailIdInCorrectFormatShouldReturnTrue() {
+    void givenEmailIdInCorrectFormatShouldReturnTrue(){
         test = new UserRegistrations();
-        boolean actual = test.email("abc.xyz@bl.co.in");
+        boolean actual = false;
+        try {
+            actual = test.email("abc.xyz@bl.co.in");
+        } catch (Exception e) {
+            System.out.println("Invalid Input "+e.getMessage());
+        }
         Assertions.assertEquals(true,actual);
     }
     @Test
-    void givenphoneNumberInCorrectFormatShouldReturnTrue() {
+    void givenphoneNumberInCorrectFormatShouldReturnTrue(){
         test = new UserRegistrations();
-        boolean actual = test.phoneNumber("91 9848372837");
+        boolean actual = false;
+        try {
+            actual = test.phoneNumber("91 9848372837");
+        } catch (Exception e) {
+            System.out.println("Invalid Input "+e.getMessage());
+        }
         Assertions.assertEquals(true,actual);
     }
     @Test
-    void givenRulePasswordInCorrectFormatShouldReturnTrue() {
+    void givenRulePasswordInCorrectFormatShouldReturnTrue(){
         test = new UserRegistrations();
-        boolean actual = test.password("Abcd@432");
+        boolean actual = false;
+        try {
+            actual = test.password("Abcd@432");
+        } catch (Exception e) {
+            System.out.println("Invalid Input "+e.getMessage());
+        }
         Assertions.assertEquals(true,actual);
     }
     @Test
