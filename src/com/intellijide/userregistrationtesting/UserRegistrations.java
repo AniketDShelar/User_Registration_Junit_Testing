edd375496bd1a8840facd7ad25e422cb3bcfd3f5
AniketDShelar <theaniketshelar@gmail.com>
2022-09-12 12:29:27 +0530

    lambda expression

--- src/com/intellijide/userregistrationtesting/UserRegistrations.java
+++ src/com/intellijide/userregistrationtesting/UserRegistrations.java
@@ -3,15 +3,17 @@ package com.intellijide.userregistrationtesting;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.intellijide.userregistrationtesting.UserRegistrationvalidation.UserRegistration.firstNameValidation;
+
 @FunctionalInterface
 interface UserRegistrationvalidation<T> {
     boolean validation(T parameter);
 
     public class UserRegistration{
         public static boolean firstNameValidation(String firstName){
-            UserRegistrationvalidation<String> obj = (fun1) ->{
+            UserRegistrationvalidation<String> obj = (fun) ->{
                 Pattern pattern1 = Pattern.compile("^[A-Z][a-z]{2,}");
-                Matcher matcher1 = pattern1.matcher(fun1);
+                Matcher matcher1 = pattern1.matcher(fun);
                 if(matcher1.matches()){
                     System.out.println(true);
                 }else{
@@ -49,9 +51,9 @@ interface UserRegistrationvalidation<T> {
         return obj.validation(email);
     }
     public static boolean mobNumberValidation(String mobNumber){
-        UserRegistrationvalidation<String> obj = (fun2) ->{
+        UserRegistrationvalidation<String> obj = (fun) ->{
             Pattern pattern4 = Pattern.compile("^[0-9]{2,}[\s][6-9][0-9]{9}$");
-            Matcher matcher4 = pattern4.matcher(fun2);
+            Matcher matcher4 = pattern4.matcher(fun);
             if(matcher4.matches()){
                 System.out.println(true);
             }else{
@@ -62,9 +64,9 @@ interface UserRegistrationvalidation<T> {
         return obj.validation(mobNumber);
     }
     public static boolean passwordValidation(String password){
-        UserRegistrationvalidation<String> obj = (fun2) ->{
+        UserRegistrationvalidation<String> obj = (fun) ->{
             Pattern pattern5 = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=[^@#!$%^&+=]*[@#!$%^&+=][^@#!$%^&+=]*$)(?=.*[0-9]).{8,}$");
-            Matcher matcher5 = pattern5.matcher(fun2);
+            Matcher matcher5 = pattern5.matcher(fun);
             if(matcher5.matches()){
                 System.out.println(true);
             }else{
@@ -76,8 +78,8 @@ interface UserRegistrationvalidation<T> {
     }
 
     public static void main(String[] args) {
-        UserRegistration obj1 = new UserRegistration();
-        obj1.firstNameValidation("Aniket");
+
+        firstNameValidation("Aniket");
         lastNameValidation("Shelar");
         mobNumberValidation("8292938293");
         emailValidation("abc.xyz@bl.co.in");
